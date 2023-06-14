596bd9ac4860b4007d9915867ded77368daec2a2
pijusdas <pijusdas.dev@gmail.com>
2023-06-14 14:21:52 +0600

    fixed deploy related bug

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
 
 
         const verfyAdmin = async (req, res, next) => {
